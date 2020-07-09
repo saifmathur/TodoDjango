43f56e38a474c03b60107b2ea0852b67acba0549
saifmathur <saifmathur41@gmail.com>
2020-07-09 19:28:16 +0530

    served static files

---
+++ todos/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Todo
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()[:10]
+    context = {
+        'todos': todos
+    }
+    return render(request,'index.html',context)
+
+def details(request, todo_id):
+    todo = Todo.objects.get(id = todo_id)
+    context = {
+        'todo':todo
+    }
+    return render(request,'details.html',context)
